7dd5d1537e3a5091c4a564568b0e96cbe2b4babb
Cyril Moreau <cymo@odoo.com>
2026-07-01 08:05:50 +0200

    [FIX] packages: fix ncspot issues
    ncspot kept crashing after few minutes of usage.
    Trying to fix it by changing from pipewire to pulseaudio.

--- home-manager/packages.nix
+++ home-manager/packages.nix
@@ -4,32 +4,29 @@ let
   zen-browser = inputs.zen-browser.packages.${pkgs.system}.default;
 in {
   home.packages = with pkgs; [
-    ### Config
-    pipewire 
-
     ### Fonts
     jetbrains-mono
     
     ### Terminal-based
     fastfetch
     btop
-    (writeShellScriptBin "ncspot" ''                                                                                                                                                           
-      export ALSA_PLUGIN_DIR=${pipewire}/lib/alsa-lib                                                                                                                                          
-      export LD_LIBRARY_PATH=${pipewire}/lib                                                                                                                                                   
-      exec ${ncspot}/bin/ncspot "$@"                                                                                                                                                           
-    '')
+    ncspot
     eza                                                                                                                                                                                  
 
     ### Development
     uv
 
-
     ### Apps
     signal-desktop
     calibre
-    obsidian
+    (obsidian.override { commandLineArgs = "--no-sandbox"; }) # Sandbox needs extra permissions, disable it for now
     # zen-browser
     # discord
-    
+    # super-productivity
+    # ferdium
   ];
+
+  xdg.configFile."ncspot/config.toml".text = ''
+    backend = "pulseaudio"
+  '';
 }
